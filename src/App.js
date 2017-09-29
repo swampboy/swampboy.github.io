da22ed12c2259035e927bb60b862e1961885696d
swampboy <butchdawson.website@gmail.com>
2017-09-28 23:10:00 -0400

    Change to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Route
 } from 'react-router-dom';
 import store from './Redux/store';
